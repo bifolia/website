4f7db43fd815967d60494f560779e4d28c82567b
Jonas Hübotter <jonas.huebotter@gmail.com>
2021-04-04 13:31:24 +0200

    [#15] Mobile menu and footer (#25)

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,14 +1,83 @@
 import './Header.scss'
+import React, { useCallback, useEffect, useRef, useState } from 'react'
+import { Arrow } from './Arrow'
 import { Link } from 'gatsby'
 import { Logo } from './Logo'
-import { Menu } from './Menu'
-import React from 'react'
+import classNames from 'classnames'
+import useOnClickOutside from 'use-onclickoutside'
 
-export const Header = () => (
-  <header className="Header">
-    <Link to="/">
-      <Logo />
-    </Link>
-    <Menu />
-  </header>
-)
+const PEEK_THRESHOLD = 50
+
+export const Header = () => {
+  const [expanded, setExpanded] = useState(false)
+
+  const collapse = useCallback(() => setExpanded(false), [setExpanded])
+  const toggle = useCallback(() => setExpanded(!expanded), [
+    expanded,
+    setExpanded,
+  ])
+
+  const [peek, setPeek] = useState(true)
+  useEffect(() => {
+    const onScroll = () => {
+      if (window.scrollY <= PEEK_THRESHOLD) setPeek(true)
+      else setPeek(false)
+    }
+
+    window.addEventListener('scroll', onScroll)
+    return () => {
+      window.removeEventListener('scroll', onScroll)
+    }
+  }, [setPeek])
+
+  const ref = useRef(null)
+  useOnClickOutside(ref, collapse)
+
+  return (
+    <header className={classNames('Header', { expanded, peek })}>
+      <div className="Header__backdrop"></div>
+
+      <div className="Header__logo">
+        <Link to="/">
+          <Logo />
+        </Link>
+      </div>
+
+      <div className="Header__menu" ref={ref}>
+        <span className="Header__menu__toggle link" onClick={toggle}>
+          <Arrow />
+        </span>
+
+        <div className="Header__menu__wrapper">
+          <Link to="/raumgestaltung" onClick={collapse}>
+            Raumgestaltung
+          </Link>
+          <br />
+          <Link to="/pflanzplanung" onClick={collapse}>
+            Pflanzplanung
+          </Link>
+          <br />
+          <Link to="/gartenentwicklung" onClick={collapse}>
+            Gartenentwicklung
+          </Link>
+          <br />
+
+          <div className="Header__menu__wrapper__expanded">
+            <Link to="/projekte" onClick={collapse}>
+              Projekte
+            </Link>
+            <br />
+            <Link to="/journal" onClick={collapse}>
+              Journal
+            </Link>
+            <br />
+            <Link to="/team" onClick={collapse}>
+              Team
+            </Link>
+            <br />
+          </div>
+        </div>
+      </div>
+    </header>
+  )
+}
