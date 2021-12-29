f82d9760a48d3c45c2ae92215320cebd20e3f71b
Jonas Hübotter <jonas.huebotter@gmail.com>
2021-12-29 18:59:22 +0100

    fixes

--- gatsby-node.js
+++ gatsby-node.js
@@ -100,3 +100,13 @@ exports.createPages = async ({ graphql, actions }) => {
     })
   })
 }
+
+exports.onCreateWebpackConfig = ({ actions, stage, plugins }) => {
+  if (stage === 'build-javascript' || stage === 'develop') {
+    actions.setWebpackConfig({
+      plugins: [
+        plugins.provide({ process: 'process/browser' })
+      ]
+    })
+  }
+}
