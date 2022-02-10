0f78c3ec3dd329c1480d61a242c53bcc8d39ab2d
Jonas Hübotter <jonas.huebotter@gmail.com>
2022-02-10 12:18:52 +0100

    reduce image size

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -53,7 +53,7 @@ export const query = graphql`
                   localFile {
                     childImageSharp {
                       gatsbyImageData(
-                        width: 3840
+                        width: 1920
                         quality: 100
                         placeholder: BLURRED
                         formats: [AUTO, WEBP]
